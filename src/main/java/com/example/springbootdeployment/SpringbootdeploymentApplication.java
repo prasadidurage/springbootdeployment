32459ba74205db8bc0675ddcfcb1260e56f6d141
Prasadi Durage <ps7032962@gamail.com>
2025-08-04 10:03:09 +0530

    add new

---
+++ src/main/java/com/example/springbootdeployment/SpringbootdeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdeploymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootdeploymentApplication.class, args);
+    }
+
+}
